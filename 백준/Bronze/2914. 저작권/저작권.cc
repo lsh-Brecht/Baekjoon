a4b088545f49a6fe9078bb3080cb5b3c4c635299
LEE SangHoon <152514399+lsh-Brecht@users.noreply.github.com>
2026-01-12 22:38:35 +0900

    [Bronze III] Title: 저작권, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+void solve() {
+	int a, b; cin >> a >> b; cout << a * b - (a - 1);
+}
+int main() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	solve();
+	return 0;
+}
