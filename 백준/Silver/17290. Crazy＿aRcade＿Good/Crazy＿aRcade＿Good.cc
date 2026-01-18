cfdfbaaf00bb01d54afe794016933b215d23cf7b
LEE SangHoon <152514399+lsh-Brecht@users.noreply.github.com>
2026-01-18 23:01:33 +0900

    [Silver V] Title: Crazy_aRcade_Good, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/17290. Crazy＿aRcade＿Good/Crazy＿aRcade＿Good.cc
+++ 백준/Silver/17290. Crazy＿aRcade＿Good/Crazy＿aRcade＿Good.cc
@@ -1,51 +1,23 @@
 #include <iostream>
-#include <vector>
 #include <string>
 #include <algorithm>
 using namespace std;
 using ll = long long;
 
-char mat[11][11];
-int dx[] = { -1, 1, 0, 0 };
-int dy[] = { 0, 0, -1, 1 };
-pair<int, int> player;
-vector<pair<int, int>> bombs;
-
 void solve() {
-	cin >> player.first >> player.second;
-	player.first--;
-	player.second--;
-	for (int i = 0; i < 10; ++i) {
-		string s;
-		cin >> s;
-		for (int j = 0; j < 10; ++j) {
-			mat[i][j] = s[j];
-			if(mat[i][j] == 'o') {
-				bombs.push_back({ i, j });
-			}
-		}
-	}
-	for(auto b : bombs) {
-		for (int k = 0; k < 4; ++k) {
-			for (int i = 0; i < 10; ++i) {
-				int nx = b.first + dx[k] * i;
-				int ny = b.second + dy[k] * i;
-				if (nx < 0 || nx > 9 || ny < 0 || ny > 9) break;
-				mat[nx][ny] = 'o';
-			}
-		}
-	}
-
-	int minv = 1000;
-	for (int i = 0; i < 10; ++i) {
-		for (int j = 0; j < 10; ++j) {
-			if (mat[i][j] == 'x') {
-				int dist = abs(player.first - i) + abs(player.second - j);
-				minv = min(minv, dist);
-			}
-		}
-	}
-	cout << minv;
+    int r, c, R = 0, C = 0, v = 99;
+    string s;
+    cin >> r >> c;
+    for (int i = 0; i < 10; ++i) {
+        cin >> s;
+        for (int j = 0; j < 10; ++j)
+            if (s[j] == 'o') R |= (1 << i), C |= (1 << j);
+    }
+    for (int i = 0; i < 10; ++i)
+        for (int j = 0; j < 10; ++j)
+            if (!(R & (1 << i)) && !(C & (1 << j)))
+                v = min(v, abs(r - 1 - i) + abs(c - 1 - j));
+    cout << v;
 }
 
 int main() {
