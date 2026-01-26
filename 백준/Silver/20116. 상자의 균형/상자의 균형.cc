c1c5d332eff29c63ff33f645266ac9d645867ebc
LEE SangHoon <152514399+lsh-Brecht@users.noreply.github.com>
2026-01-26 13:31:14 +0900

    [Silver III] Title: 상자의 균형, Time: 24 ms, Memory: 3584 KB -BaekjoonHub

--- 백준/Silver/20116. 상자의 균형/상자의 균형.cc
+++ 백준/Silver/20116. 상자의 균형/상자의 균형.cc
@@ -3,25 +3,21 @@
 using namespace std;
 using ll = long long;
 vector<ll> boxes;
-vector<ll> accSum;
 void solve() {
 	int n, s; cin >> n >> s;
-	boxes.resize(n);  accSum.resize(n);
+	boxes.resize(n);
 	for (int i = 0; i < n; ++i) {
 		cin>> boxes[i];
 	}
-	accSum[n - 1] = boxes[n - 1];
-	for (int i = n-2; i >= 0; --i) {
-		accSum[i] = boxes[i] + accSum[i + 1];
-	}
-	bool f = true;
+	ll sum = 0;
 	for (int i = n - 1; i > 0; --i) {
-		if (accSum[i] <=(boxes[i-1]-s)*(n - i) || (boxes[i-1]+s)*(n - i) <= accSum[i]) {
-			f = false;
-			break;
+		sum += boxes[i];
+		if (sum <=(boxes[i-1]-s)*(n - i) || (boxes[i-1]+s)*(n - i) <= sum) {
+			cout << "unstable";
+			return;
 		}
 	}
-	cout << (f ? "stable" : "unstable");
+	cout << "stable";
 }
 int main() {
 	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
