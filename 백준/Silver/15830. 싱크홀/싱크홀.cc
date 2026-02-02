253a2e039c2675f9e666fa3fb12075434e3573dd
LEE SangHoon <152514399+lsh-Brecht@users.noreply.github.com>
2026-02-02 21:27:32 +0900

    [Silver IV] Title: 싱크홀, Time: 0 ms, Memory: 2020 KB, Score: 150 point -BaekjoonHub

--- 백준/Silver/15830. 싱크홀/싱크홀.cc
+++ 백준/Silver/15830. 싱크홀/싱크홀.cc
@@ -5,28 +5,25 @@
 using namespace std;
 using ll = long long;
 
+//질문게시판 확인
 void solve()
 {
-	int v, W, D;
-	cin >> v >> W >> D;
-	double falling = 0.0;
-	double V = v;
-	int count = 0;
-	while (true)
-	{
-		//벽에 충돌할 때까지 걸리는 시간
-		double t = W / V;
-		double dy = 5.0 * t * t;
-		//그동안 바닥까지 떨어졌다면
-		if (falling + dy >= D)
-		{
-			break;
-		}
-		falling += dy;
-		++count;
-		V *= 0.8;
-	}
-	cout << count;
+	double V, W, D;
+	cin >> V >> W >> D;
+    int count = 0;
+    double falling = 0.0;
+    double dy = 5.0L * (W / V) * (W / V);
+    while (true)
+    {
+        if (falling + dy >= D)
+        {
+            break;
+        }
+        falling += dy;
+        count++;
+        dy *= 1.5625;
+    }
+    cout << count;
 }
 int main()
 {
