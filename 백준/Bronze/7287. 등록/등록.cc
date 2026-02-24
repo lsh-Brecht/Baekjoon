90a8abbb95c4daabb46884011c4fde3666426dcb
LEE SangHoon <152514399+lsh-Brecht@users.noreply.github.com>
2026-02-24 13:42:52 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+using namespace std;
+using ll = long long;
+void solve()
+{
+	cout << 802 << '\n' << "brecht3000";
+}
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	solve();
+	return 0;
+}
