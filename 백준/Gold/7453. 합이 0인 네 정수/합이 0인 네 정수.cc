85ec9f1e27fa6f651a8088827265a43ec6ddeac5
LEE SangHoon <152514399+lsh-Brecht@users.noreply.github.com>
2026-03-02 16:37:31 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 10448 ms, Memory: 694304 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
@@ -4,41 +4,42 @@
 #include <algorithm>
 #include <cmath>
 #include <iomanip>
+#include <unordered_map>
 using namespace std;
 using ll = long long;
-
 int n, A[4000], B[4000], C[4000], D[4000], S[16000000];
 int k;
 ll ans;
-int count_val(int t)
-{
-	auto low = lower_bound(S, S + k, t);
-	auto up = upper_bound(S, S + k, t);
-	return up - low;
-}
 void solve()
 {
 	cin >> n;
-	for (int i = 0; i < n; i++)
-	{
-		cin >> A[i] >> B[i] >> C[i] >> D[i];
-	}
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			S[k++] = A[i] + B[j];
-		}
-	}
-	sort(S, S + k);
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			ans += count_val(-C[i] - D[j]);
-		}
-	}
-	cout << ans;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> A[i] >> B[i] >> C[i] >> D[i];
+    }
+    unordered_map<int, int> sumAB;
+    sumAB.reserve(n * n / 2);
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            sumAB[A[i] + B[j]]++;
+        }
+    }
+    ll ans = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            int target = -(C[i] + D[j]);
+            auto it = sumAB.find(target);
+            if (it != sumAB.end())
+            {
+                ans += it->second;
+            }
+        }
+    }
+    cout << ans;
 }
 int main()
 {
