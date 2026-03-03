7da7194c3538eaa34df56a981c3263aa0deee789
LEE SangHoon <152514399+lsh-Brecht@users.noreply.github.com>
2026-03-03 17:20:57 +0900

    [Gold I] Title: 2048 (Easy), Time: 20 ms, Memory: 2552 KB -BaekjoonHub

---
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cc
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue> //이 문제를 드디어 푸네
+using namespace std;
+using ll = long long;
+
+int n;
+int dx[4] = { 0,0,-1,1 };
+int dy[4] = { 1,-1,0,0 };
+enum DIR { UP, DOWN, LEFT, RIGHT };;
+struct mat
+{
+	int cnt;
+	int maxv;
+	int matrix[20][20];
+};
+mat moving(mat m, int dir)
+{
+    // 0: UP, 1: DOWN, 2: LEFT, 3: RIGHT
+    for (int k = 0; k < n; ++k)
+    {
+        vector<int> v;
+        for (int i = 0; i < n; ++i)
+        {
+            int r, c;
+            if (dir == UP)
+            {
+                r = i; c = k;
+            }
+            else if (dir == DOWN)
+            {
+                r = n - 1 - i; c = k;
+            }
+            else if (dir == LEFT)
+            {
+                r = k; c = i;
+            }
+            else
+            {
+                r = k; c = n - 1 - i;
+            }
+            if (m.matrix[r][c] != 0)
+            {
+                v.push_back(m.matrix[r][c]);
+                m.matrix[r][c] = 0;
+            }
+        }
+        vector<int> merged;
+        for (int i = 0; i < v.size(); ++i)
+        {
+            if (i + 1 < v.size() && v[i] == v[i + 1])
+            {
+                merged.push_back(v[i] * 2);
+                i++;
+            }
+            else
+            {
+                merged.push_back(v[i]);
+            }
+        }
+
+        for (int i = 0; i < merged.size(); ++i)
+        {
+            int r, c;
+            if (dir == UP)
+            {
+                r = i; c = k;
+            }
+            else if (dir == DOWN)
+            {
+                r = n - 1 - i; c = k;
+            }
+            else if (dir == LEFT)
+            {
+                r = k; c = i; }
+            else
+            {
+                r = k; c = n - 1 - i;
+            }
+            m.matrix[r][c] = merged[i];
+        }
+    }
+
+    int cur_max = 0;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cur_max = max(cur_max, m.matrix[i][j]);
+    m.maxv = cur_max;
+
+    return m;
+}
+
+queue<mat> q;
+void solve()
+{
+	cin >> n;
+	mat start;
+	for (int i = 0; i < n; ++i)
+	{
+		int temp;
+		for (int j = 0; j < n; ++j)
+		{
+			cin >> temp;
+			start.matrix[i][j] = temp;
+		}
+	}
+	start.cnt = 0;
+	q.push(start);
+	int maxv = -1;
+	while (!q.empty())
+	{
+		mat cur = q.front(); q.pop();
+		for (int i = 0; i < 4; ++i)
+		{
+			mat ret = moving(cur, i);
+			maxv = max(maxv, ret.maxv);
+			ret.cnt = cur.cnt + 1;
+			if (ret.cnt <5) q.push(ret);
+		}
+	}
+	cout << maxv;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	solve();
+	return 0;
+}
